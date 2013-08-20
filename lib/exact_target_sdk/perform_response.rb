85d12ddebcd516c9aa2cc831495e9b39797e7334
Corey Pierson <corey@Coreys-iMac.local>
2013-08-20 12:53:55 -0400

    add results to perform_response to help with trouble shooting potential errors

--- lib/exact_target_sdk/perform_response.rb
+++ lib/exact_target_sdk/perform_response.rb
@@ -8,6 +8,18 @@ class PerformResponse
     @OverallStatus = response[:overall_status]
     @OverallStatusMessage = response[:overall_status_message]
     @RequestID = response[:request_id]
+    @Results = []
+
+    results = if response[:results].is_a? Array
+      response[:results]
+    elsif response[:results].is_a? Hash
+      [ response[:results] ]
+    else
+      []
+    end
+    results.each do |result|
+      @Results << CreateResult.new(result)
+    end
   end
 
 end
